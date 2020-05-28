bb2b70a6d4d92c818db79da5518c3607c1913dfc
Lego-zhang <1945465017@qq.com>
2020-05-28 14:06:52 +0800

    使用immutable.js来管理store中的数据

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { CSSTransition } from "react-transition-group";
+import { actionCreators } from "./store";
 import {
   HeaderWrapper,
   Logo,
@@ -49,22 +50,16 @@ const Header = (props) => {
 };
 const mapStateToProps = (state) => {
   return {
-    focused: state.focused,
+    focused: state.header.get("focused"),
   };
 };
 const mapDispathToProps = (dispath) => {
   return {
     handleInputFocus() {
-      const action = {
-        type: "search_focus",
-      };
-      dispath(action);
+      dispath(actionCreators.searchFocus());
     },
     handleInputBlur() {
-      const action = {
-        type: "search_blur",
-      };
-      dispath(action);
+      dispath(actionCreators.searchBlur());
     },
   };
 };
